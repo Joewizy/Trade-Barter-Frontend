48d900954c61508830f33f408dfb7aeeac0f6287
mac <joewigimbasin@gmail.com>
2025-05-22 14:54:29 +0100

    fixed-npm-issues

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -3,11 +3,12 @@
 import { WalletProvider } from "@suiet/wallet-kit"
 import "@suiet/wallet-kit/style.css";
 import "./custom-styles.css"
+import React from 'react';
 
 export default function WalletProviderWrapper({ children }: { children: React.ReactNode }) {
   return (
     <WalletProvider>
-      {children}
+      {children as any}
     </WalletProvider>
   )
 }
